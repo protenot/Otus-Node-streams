dfa5ea235306502510133a382c03aa8af72f1abf
Olga_Belaya <protenot@yandex.ru>
2023-11-27 07:32:45 +0300

    functions and pipeline done

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 const fs = require("fs");
-
-(async () => {
-  const readStream = fs.createReadStream("input-text", {
-    encoding: "utf8",
-  });
-  const writeStream = fs.createWriteStream("output-text", {
-    encoding: "utf8",
-  });
-  let data = "";
-  for await (const item of readStream) {
-    data += item;
-  }
-
-  console.log(data);
-
-  const words = data
+const { Transform, pipeline } = require("stream");
+//(async () => {
+function extractWords(data) {
+  return data
     .toLowerCase()
     .replace(/[^\w\s]/g, "")
     .split(" ")
     .filter((word) => word !== "");
-  console.log(words);
+}
 
+function countWords(words) {
   const wordCounts = {};
   words.forEach((word) => {
     if (wordCounts[word]) {
@@ -29,13 +18,35 @@ const fs = require("fs");
       wordCounts[word] = 1;
     }
   });
-  const sortedWordCounts = Object.entries(wordCounts)
+
+  return Object.entries(wordCounts)
     .sort((a, b) => a[0].localeCompare(b[0]))
     .map((entry) => entry[1]);
+}
 
-  console.log(sortedWordCounts);
-  const resultString = sortedWordCounts.join(" ");
-  writeStream.write(resultString);
-  writeStream.end();
-  console.log(`Результат ${resultString} и он записан в файл output-text`);
-})();
+function formatResult(sortedWordCounts) {
+  return sortedWordCounts.join(" ");
+}
+const readStream = fs.createReadStream("input-text", {
+  encoding: "utf8",
+});
+const writeStream = fs.createWriteStream("output-text", {
+  encoding: "utf8",
+});
+
+const transformStream = new Transform({
+  transform(chunk, encoding, callback) {
+    const words = extractWords(chunk.toString());
+    const counts = countWords(words);
+    const resultString = formatResult(counts);
+    this.push(resultString);
+    callback();
+  },
+});
+pipeline(readStream, transformStream, writeStream, (err) => {
+  if (err) {
+    console.log("Pipeline failed; " + err);
+  } else {
+    console.log("Success!!!");
+  }
+});
